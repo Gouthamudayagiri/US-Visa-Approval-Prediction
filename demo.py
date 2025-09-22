963e51cd19daca331b6b9774925309bc7693e49c
Gouthamudayagiri <gouthamudayagiri@gmail.com>
2025-09-22 19:56:30 +0530

    exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 # from us_visa.logger import logging
 # logging.info("Welcome to our custom logging")
 
+from us_visa.exception import USvisaException
+import sys
+
+try:
+    a =1/"10"
+except Exception as e:
+    raise USvisaException(e,sys) from e

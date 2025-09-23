d1c8f9cf3fa67168631899128398421189d6ac02
Gouthamudayagiri <gouthamudayagiri@gmail.com>
2025-09-23 20:45:34 +0530

    Constants added

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 from us_visa.logger import logging
-# logging.info("Welcome to our custom logging")
+logging.info("Welcome to our custom logging")
 
-from us_visa.exception import USvisaException
-import sys
+# from us_visa.exception import USvisaException
+# import sys
 
-try:
-    a =1/"10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e,sys) from e
+# try:
+#     a =1/"10"
+# except Exception as e:
+#     logging.info(e)
+#     raise USvisaException(e,sys) from e
